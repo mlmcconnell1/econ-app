5de7d06a1adc4fbf44a040e0ca6960178e7c0542
Matt McConnell <matt@coolescence.com>
2025-02-23 09:19:11 -0700

    Check for running on Replit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
 
+# Will be True on Replit, False elsewhere
+is_replit = bool(os.getenv('REPL_ID'))
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +10,7 @@ def index():
     return render_template('index.html')  # Serves the frontend
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Runs the local server
+    if is_replit:
+        app.run(host='0.0.0.0', port=5000)
+    else:
+        app.run(debug=True)  # Runs the local server in debug mode
